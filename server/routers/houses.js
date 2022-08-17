e3d7675057f670218104caa85d00958b06c0aec4
KMV <karl-martin.vogel@tc.de>
2022-08-17 14:11:26 +0200

    dv

--- server/routers/houses.js
+++ server/routers/houses.js
@@ -11,9 +11,37 @@ router.get('/new', (req, res) => {
 router.post('/', (req, res) => {
     res.send('create house')
 })
-router.get("/:id", (req, res) => {
-    req.params.id
-    res.send(`get house with ID ${req.params.id}`)
+//kompliziert:
+// router.get("/:houseId", (req, res) => {
+//     res.send(`get house with ID ${req.params.houseId}`) //still works lol
+// })
+// router.put("/:houseId", (req, res) => {
+//     res.send(`put house with ID ${req.params.houseId}`)
+// })
+// router.delete("/:houseId", (req, res) => {
+//     res.send(`delete house with ID ${req.params.houseId}`)
+// })
+// geht einfacher unten mit router.route:
+router.route("/:houseId")
+    .get((req, res) => {
+        console.log(req.user)
+        res.send(`get house with ID ${req.params.houseId}, user: ${req.user}`)
+    })
+    .put( (req, res) => {
+        res.send(`put house with ID ${req.params.houseId}`)
+    })
+    .delete((req, res) => {
+        res.send(`delete house with ID ${req.params.houseId}`)
+    })
+//
+const users = [{name:"Peter"}, {name: "Sarah"}]
+router.param("houseId", (req, res, next, houseId) => {
+    //whenever an url with that dynamic parameter (houseId) is called
+    console.log(houseId) //runs this code indefinitely until 'next' function is called
+    // aka MIDDLEWARE - runs between the request to server and the actual response being sent
+    // this concept allows us to load e.g. variables and then let them be processed by backend:
+    req.user = users[houseId]
+    next() // allows to run the next functions now, aka get, put, delete etc.
 })
 
 module.exports = router
